ed35ba108623b3f7bbe87e8e004fb80e99060132
Noah <npizza10@gmail.com>
2020-11-15 01:53:05 -0700

    first commit

---
+++ ResearchProject_TennisPredictor.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 15 01:47:23 2020
+
+@author: R Noah Padilla
+
+- Clean Data
+- Choose an Algo
+- Run it
+"""
+
+from utils import *
+
+if __name__ == "__main__":
+
+    print("Tennis Tournament Predictor")
+
+
